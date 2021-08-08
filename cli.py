b71749490315cc0e9363ef31e4e94f6cb9466970
Max Walters <max.walters10@gmail.com>
2021-08-07 22:38:57 -0400

    Creature vs !Creature handling

--- cli.py
+++ cli.py
@@ -15,28 +15,39 @@ def cardSearch():
     
     cardsReturned = len(data['data'])
     
-    print(f"Your search \"{searchQuery}\" returned {int(cardsReturned)} card(s):")
+    print(f"Your search \"{searchQuery}\" returned {int(cardsReturned)} card(s):\n")
     
     for i in range(cardsReturned):
         print(f"#{i+1}: {data['data'][i]['name']}")
         results[i] = f"{data['data'][i]['uri']}"
         
-    selection = input("Choose a card number to see more info about that card: ")
+    selection = input("\nChoose a card number to see more info about that card: ")
     selectedResultURI = results[int(selection) - 1]
     cardPrintout(selectedResultURI)
     
 def cardPrintout(cardURI):
     jsonURL = urllib.request.urlopen(cardURI)
     data = json.loads(jsonURL.read())
-    cardData = {
-        "name": f"{data['name']}",
-        "mana_cost": f"{data['mana_cost']}",
-        "type_line": f"{data['type_line']}",
-        "oracle_text": f"{data['oracle_text']}",
-        "power": f"{data['power']}",
-        "toughness": f"{data['toughness']}",
-    }
-    rprint(f"\n[b]{cardData['name']}[/b] | {cardData['mana_cost']} | {cardData['power']}/{cardData['toughness']}\n[u]{cardData['type_line']}[/u]\n{cardData['oracle_text']}\n")
+    hasPower = bool(data.get('power'))
+    
+    if hasPower:
+        cardData = {
+            "name": f"{data['name']}",
+            "mana_cost": f"{data['mana_cost']}",
+            "type_line": f"{data['type_line']}",
+            "oracle_text": f"{data['oracle_text']}",
+            "power": f"{data['power']}",
+            "toughness": f"{data['toughness']}",
+        }
+        rprint(f"\n[b]{cardData['name']}[/b] | {cardData['mana_cost']} | {cardData['power']}/{cardData['toughness']}\n[u]{cardData['type_line']}[/u]\n{cardData['oracle_text']}\n".replace("{T}", "{Tap}"))
+    else:
+        cardData = {
+            "name": f"{data['name']}",
+            "mana_cost": f"{data['mana_cost']}",
+            "type_line": f"{data['type_line']}",
+            "oracle_text": f"{data['oracle_text']}",
+        }
+        rprint(f"\n[b]{cardData['name']}[/b] | {cardData['mana_cost']}\n[u]{cardData['type_line']}[/u]\n{cardData['oracle_text']}\n".replace("{T}", "{Tap}"))
     
 if __name__ == '__main__':
   fire.Fire()

54aa4382e1078fb8e5dacf9d62267131e0575a6a
Max Walters <max.walters10@gmail.com>
2021-08-07 23:16:07 -0400

    cardSearch edit & add exactCard

--- cli.py
+++ cli.py
@@ -1,12 +1,13 @@
 import fire
 import json
 import urllib.request
+from urllib.error import HTTPError
 from rich import print as rprint
 
 url = "https://api.scryfall.com/"
 
 # Returns list of cards found using a search string
-# Currently only returns first "page" of results (175 cards)
+# Currently only returns first "page" of results (175 cards) sorted by name A -> Z
 def cardSearch():
     results = {}
     
@@ -17,15 +18,35 @@ def cardSearch():
     
     cardsReturned = len(data['data'])
     
-    print(f"Your search \"{searchQuery}\" returned {int(cardsReturned)} card(s):\n")
+    if cardsReturned == 1:
+        print(f"Your search \"{searchQuery}\" returned 1 card:")
+        cardPrintout(data['data'][0]['uri'])
+    else:
+        print(f"Your search \"{searchQuery}\" returned {int(cardsReturned)} card(s):\n")
     
-    for i in range(cardsReturned):
-        print(f"#{i+1}: {data['data'][i]['name']}")
-        results[i] = f"{data['data'][i]['uri']}"
-        
-    selection = input("\nChoose a card number to see more info about that card: ")
-    selectedResultURI = results[int(selection) - 1]
-    cardPrintout(selectedResultURI)
+        for i in range(cardsReturned):
+            print(f"#{i+1}: {data['data'][i]['name']}")
+            results[i] = f"{data['data'][i]['uri']}"
+            
+        selection = input("\nChoose a card number to see more info about that card: ")
+        selectedResultURI = results[int(selection) - 1]
+        cardPrintout(selectedResultURI)
+
+# Returns card printout if exact card is matched via the API, otherwise throws an error and restarts.    
+def exactCard():
+    rprint("[b]This function only returns a card when the name is exact (case-insensitive and optional punctuation).\nIt WILL return an error if no card is found.[/b]")
+    searchQuery = input("\nPlease input an exact card name: ")
+    searchURL = f"{url}cards/named?exact={searchQuery}".lower().replace(" ", "+")
+    try:
+        jsonURL = urllib.request.urlopen(searchURL)
+    except HTTPError as err:
+        if err.code == 404:
+            rprint("[b]Card not found, please try again.[/b]\n")
+            exactCard()
+    else:
+        data = json.loads(jsonURL.read())
+    
+    cardPrintout(data['uri'])
     
 # Prints out card name, mana cost, type, text, and (if applicable) power and toughness
 def cardPrintout(cardURI):
